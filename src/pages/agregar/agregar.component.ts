eb3a4f210093d1132e5da57f6f3f9acf46e4996a
Javier Fernandez <ljavierfm@gmail.com>
2018-04-19 22:41:17 +0200

    Agregar items a lista

--- src/pages/agregar/agregar.component.ts
+++ src/pages/agregar/agregar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Lista,ListaItem } from '../../app/clases/index';
+
 
 @Component({
     selector: 'app-agregar',
@@ -7,7 +9,19 @@ import { Component, OnInit } from '@angular/core';
 export class AgregarComponent implements OnInit {
     nombreLista:string;
     nombreItem:string;
+    items:ListaItem[]=[];
     constructor() { }
 
     ngOnInit(): void { }
+
+    agregar(){
+        if(this.nombreItem.length==0){
+            return;
+        }
+
+        let item=new ListaItem();
+        item.nombre=this.nombreItem;
+        this.items.push(item);
+        this.nombreItem='';
+    }
 }
